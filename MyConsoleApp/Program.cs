c1ee2509f0a7184a3fdd9ec0693c1d4a5804e30a
GuinhoSensei <thiggouveia@hotmail.com>
2025-10-25 11:30:44 -0700

    initial commit

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿var date = DateTime.Now;
+var daysUntilChristmas = (new DateTime(date.Year, 12, 25) - date).Days;
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is:{date}");
+Console.WriteLine($"Days until Chsitmas: {daysUntilChristmas}");
