c0076a79688cc3fff3dc3db54c7b986f6fb00a97
GuinhoSensei <thiggouveia@hotmail.com>
2025-11-04 19:46:53 -0800

    Stopped on beginning of part 8

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95ee6015722c80b6695401b456598f6e7f2d191")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cde6ef5de199e17c96271f25a6f396b2aa7755e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
